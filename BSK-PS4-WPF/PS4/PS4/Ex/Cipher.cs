95a9f6503a34d1b1ceea600cf6924eb4b2b6376d
Mateusz Pogrebniak <93048478+ShadowRaven14@users.noreply.github.com>
2023-09-16 15:03:08 +0200

    Initial commit

---
+++ BSK-PS4-WPF/PS4/PS4/Ex/Cipher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS4.Ex
+{
+    class Cipher
+    {
+        private string BitString;
+        private string Taps;
+        private string Repeat;
+        public List<int> key;
+        public List<int> seed;
+        
+        public Cipher(string BitString, string Taps)
+        {
+            this.BitString = BitString;
+            this.Taps = Taps;
+            Repeat = BitString;
+            
+        }
+        public string Encrypt()
+        {
+            string temp = Repeat.ToString();
+            LFSR LFSR = new(temp, Taps);
+            key = LFSR.Generate();
+            seed = LFSR.GetRandoms();
+            int[] bitStringsInts = LFSR.ToArray(BitString);
+            int[] resultInts = new int[BitString.Length];
+
+            for (int i = 0; i < BitString.Length; i++)
+            {
+                int xor = key[i] ^ bitStringsInts[i];
+                resultInts[i] = xor;
+            }
+
+            return string.Join("", resultInts);
+        }
+    }
+}
---
+++ BSK-PS4-WPF/PS4/PS4/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using PS4.Ex;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PS4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Generate_Click(object sender, RoutedEventArgs e)
+        {
+            LFSR lfsr = new(seedLFSR.Text, tapsLFSR.Text);
+            List<int> temp = lfsr.Generate();
+            string temptext = "";
+            foreach(int i in temp)
+            {
+                temptext = temptext + i.ToString();
+            }
+            generated.Text = temptext;
+        }
+
+        private void Encrypt_Click(object sender, RoutedEventArgs e)
+        {
+
+            Cipher cipher = new(bitString.Text, streamTaps.Text);
+            cipherResult.Text = cipher.Encrypt();
+            string temptext = "";
+            foreach (int i in cipher.seed)
+            {
+                temptext = temptext + i.ToString();
+            }
+            streamSeed.Text = temptext;
+            
+        }
+    }
+}
